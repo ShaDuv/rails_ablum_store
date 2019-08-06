e906949b1fa8304d119dda49280651a797ae7673
Alex Siegberg and Shawna Duvall <shawna@>
2019-08-06 09:54:07 -0700

    add validations, titleize, sass, bootstrap, partials.

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -15,6 +15,7 @@ class AlbumsController < ApplicationController
   def create
     @album = Album.new(album_params)
     if @album.save
+      flash[:notice] = "You fucking did it!"
       redirect_to albums_path
     else
       render :new
